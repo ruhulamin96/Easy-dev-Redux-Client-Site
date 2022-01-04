7dcdc442d149708808419fcf010f0e794b1e7820
Ruhul Amin <ruhuliuice@gmail.com>
2022-01-04 22:17:21 +0600

    remove .env file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,13 +6,4 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDDr1yGjZh7veOkpHtBizZBgCbX_o2GEdk",
-//     authDomain: "easydev-85945.firebaseapp.com",
-//     projectId: "easydev-85945",
-//     storageBucket: "easydev-85945.appspot.com",
-//     messagingSenderId: "125494102366",
-//     appId: "1:125494102366:web:2ac6db23814f535860a7cf",
-//   };
-
 export default firebaseConfig;
